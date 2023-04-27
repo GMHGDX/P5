0a9944761ff50d99566a10da4ff7b615e37e3375
GabrielleHieken <gabriellehieken@gmail.com>
2023-04-26 19:55:37 -0500

    deallocate resources

--- user_proc.c
+++ user_proc.c
@@ -32,26 +32,19 @@ int main(int argc, char *argv[]){
     double readFromMem;
     readFromMem = *shm_ptr;
 
-    printf("\nThis is the number child read from memory: %lf", readFromMem);  //TESTING
+    printf("\nCHILD: This is the number child read from memory: %lf", readFromMem);  //TESTING
 
     //message queue
     if((msqkey = ftok("oss.h", 'a')) == (key_t) -1){ perror("IPC error: ftok"); exit(1); } //get message queue key used in oss
     if ((msqid = msgget(msqkey, PERMS)) == -1) { perror("msgget in child"); exit(1); } //access oss message queue
   
-    printf("Child %d received message: %s was my message and my int data was %d\n",getpid(), buf.strData, buf.intData); //TESTING
+    printf("CHILD: Child %d received message: %s was my message and my int data was %d\n",getpid(), buf.strData, buf.intData); //TESTING
 
     //Create a random number for how many instances of each resource the process wants and add it to an array
     int i;
     for(i=0;i<10;i++){
         resourceAsk[i] = randomNumberGenerator(resourceLim); 
     }
-
-    printf("I need: "); //TESTING
-    //printing resources asked
-    for(i=0;i<10;i++){
-        printf("%i ", resourceAsk[i]); 
-    }
-    printf("\n");
 
     //concatenate all the resources requested by the process in one string
     char *together;
@@ -69,10 +62,9 @@ int main(int argc, char *argv[]){
         if(i < 9){
             strcat(together, " ");
         }
-         printf("added %s String is now %s\n", resourceAsk_string, together); //TESTING
     }
 
-    printf("These are your resources in one string format: %s\n", together); //TESTING
+    printf("CHILD: These are your resources in one string format: %s\n", together); //TESTING
 
     //copy our new string into mtext
     strcpy(buf.strData, together);
@@ -83,13 +75,13 @@ int main(int argc, char *argv[]){
 
     //send our string to message queue
     if(msgsnd(msqid, &buf, sizeof(msgbuffer), 0 == -1)){ perror("msgsnd to child 1 failed\n"); exit(1); }
-    printf("sent message %s\n", together); //TESTING
+    printf("CHILD: sent message %s\n", together); //TESTING
 
     if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) { perror("failed to receive message from parent\n"); exit(1); } // receive a message from oss, but only one for our PID
-    printf("Child %d received message: %s was my message and my int data was %d\n",getpid(), buf.strData, buf.intData); //TESTING
+    printf("CHILD: Child %d received message: %s was my message and my int data was %d\n",getpid(), buf.strData, buf.intData); //TESTING
     int checkResponse = atoi(buf.strData);
-    
-    printf("recieved %i from parent", checkResponse);
+
+    printf("CHILD: recieved checkresponse %i from parent", checkResponse);
     
     while(checkResponse != 1){
         if (msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) { perror("failed to receive message from parent\n"); exit(1); }
@@ -99,7 +91,7 @@ int main(int argc, char *argv[]){
 
     //send our string to message queue
     if(msgsnd(msqid, &buf, sizeof(msgbuffer), 0 == -1)){ perror("msgsnd to child 1 failed\n"); exit(1); }
-    printf("sent my last message o7 farewell cruel; world");
+    printf("CHILD: sent my last message o7 farewell cruel; world\n");
 
     return 0;
 }
