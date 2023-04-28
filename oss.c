5946dce68a3c6809038af0a63c1b9172afedbc92
GabrielleHieken <gabriellehieken@gmail.com>
2023-04-27 19:40:51 -0500

    end program

--- oss.c
+++ oss.c
@@ -402,17 +402,17 @@ int main(int argc, char *argv[]){
             }
             printf("\n");
 
-            // printf("RescouresLeft:");
-            // for(i=0;i<10;i++){
-            //     printf(" %i", resourcesLeft[i]);
-            // }
-            // printf("\n");
+            printf("RescouresLeft:");
+            for(i=0;i<10;i++){
+                printf(" %i", resourcesLeft[i]);
+            }
+            printf("\n");
 
-            // printf("Rescouresused:");
-            // for(i=0;i<10;i++){
-            //     printf(" %i", resourcesUsed[i]);
-            // }
-            // printf("\n");
+            printf("Rescouresused:");
+            for(i=0;i<10;i++){
+                printf(" %i", resourcesUsed[i]);
+            }
+            printf("\n");
 
 
             if(!notenoughresources){
